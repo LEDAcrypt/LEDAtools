3686f963c864fa20ff0366c8ded415b97ba9a155
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2024-07-08 18:39:12 +0200

    ADD reduction factor boolean flag to work_factor_computation

--- work_factor_computation.cpp
+++ work_factor_computation.cpp
@@ -7,17 +7,27 @@
 
 #include "binomials.hpp"
 #include "isd_cost_estimate.hpp"
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
-int main(int argc, char* argv[]){
-  if(argc != 6){
-     std::cout << "Work factor computation for ISD" << std::endl << " Usage " 
-               << argv[0] << " <codeword_size> <code_dimension> <number_of_errors> <qc_block_size> <is_kra>" << std::endl << 
-               "<qc_block_size> = 1 implies a non QC code " << std::endl << 
-               "<is_kra> = the attack is a key recovery attack on a QC-[L|M]DPC " << std::endl;
+int main(int argc, char *argv[]) {
+  if (argc != 7) {
+    std::cout
+        << "Work factor computation for ISD" << std::endl
+        << " Usage " << argv[0]
+        << " <codeword_size> <code_dimension> <number_of_errors> "
+           "<qc_block_size> <is_kra>"
+        << std::endl
+        << "<qc_block_size> = 1 implies a non QC code " << std::endl
+        << "<is_kra> = the attack is a key recovery attack on a QC-[L|M]DPC "
+        << std::endl
+        << "<is_red_factor_applied> = if the quasi-cyclic reduction factor "
+           "cost should be applied"
+        << std::endl;
     return -1;
   }
+
+  /* reduce by a factor matching the QC block size */
 
   InitBinomials();
   NTL::RR::SetPrecision(NUM_BITS_REAL_MANTISSA);
@@ -27,11 +37,24 @@ int main(int argc, char* argv[]){
   uint32_t t = atoi(argv[3]);
   uint32_t qc_block_size = atoi(argv[4]);
   uint32_t is_kra = atoi(argv[5]);
-  
-  /* reduce by a factor matching the QC block size */
-  bool subtract_red_factor = false;
-  std::cout << "Minimum classic cost :" << c_isd_log_cost(n,k,t,qc_block_size,is_kra, subtract_red_factor) << " Minimum quantum cost :" << q_isd_log_cost(n,k,t,qc_block_size,is_kra,subtract_red_factor);
-  if(qc_block_size !=1) std::cout << " (including qc_effects) ";
+  uint32_t is_red_factor_applied = atoi(argv[6]);
+
+  std::cout << " Input params: " << std::endl
+            << "- <codeword_size>: " << n << std::endl
+            << "- <code_dimension>: " << k << std::endl
+            << "- <number of errors>: " << t << std::endl
+            << "- <qc block size>: " << qc_block_size << std::endl
+            << "- <is_kra>: " << is_kra << std::endl
+            << "- <is_red_factor_applied>: " << is_red_factor_applied << std::endl;
+
+  std::cout << "Minimum classic cost :"
+            << c_isd_log_cost(n, k, t, qc_block_size, is_kra,
+                              is_red_factor_applied)
+            << " Minimum quantum cost :"
+            << q_isd_log_cost(n, k, t, qc_block_size, is_kra,
+                              is_red_factor_applied);
+  if (qc_block_size != 1)
+    std::cout << " (including qc_effects) ";
   std::cout << std::endl;
   return 0;
 }
