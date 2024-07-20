0efdd71611ab4295b603b304e9283e6325d024a7
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2024-07-20 18:32:51 +0200

    ADD global paths

---
+++ globals.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+const std::string OUT_DIR_RESULTS = "out/results/json";
+// It seems impossible in C++
+// std::string OUT_FILE_RESULT_FMT_JSON = "{n:06}_{r:06}_{t:03}.json";
+const std::string LOG_DIR = "logs/";
+const std::string LOG_FILE_DEFAULT = LOG_DIR + "default.log";
--- work_factor_computation_parallel.cpp
+++ work_factor_computation_parallel.cpp
@@ -8,14 +8,16 @@
 #include <sstream>
 #include <string>
 
-#define NUM_BITS_REAL_MANTISSA 1024
-#define IGNORE_DECODING_COST 0
-// #define EXPLORE_REPRS
-
 #include "binomials.hpp"
 #include "isd_cost_estimate.hpp"
 #include "logging.hpp"
+#include "globals.hpp"
 #include <iostream>
+#include <format> // Requires C++20
+
+#define NUM_BITS_REAL_MANTISSA 1024
+#define IGNORE_DECODING_COST 0
+// #define EXPLORE_REPRS
 
 void to_json(nlohmann::json &j, const Result &r) {
   j = nlohmann::json{
@@ -30,11 +32,13 @@ void from_json(const nlohmann::json &j, Result &r) {
 
 int main() {
   // Configure the logger
+
   configure_logger(std::nullopt);
 
+  // TODO take from input?
   std::ifstream file("out/isd_values.json");
 
-  // Check if the file is open
+  // Check if the file is open 
   if (!file.is_open()) {
     std::cerr << "Could not open the file!" << std::endl;
     return 1;
@@ -48,6 +52,19 @@ int main() {
   NTL::RR::SetPrecision(NUM_BITS_REAL_MANTISSA);
   pi = NTL::ComputePi_RR();
   bool is_kra_values[] = {true, false};
+  std::filesystem::path dirPath(OUT_DIR_RESULTS);
+  // Check if the directory exists
+  if (!std::filesystem::exists(dirPath)) {
+    // Try to create the directory, including parent directories
+    if (std::filesystem::create_directories(dirPath)) {
+      std::cout << "Directory created successfully: " << OUT_DIR_RESULTS
+                << std::endl;
+    } else {
+      std::cerr << "Failed to create directory: " << OUT_DIR_RESULTS
+                << std::endl;
+      return 1; // Return an error code
+    }
+  }
 // Iterate over the list of entries
 #pragma omp parallel for
   for (const auto &entry : j) {
@@ -61,6 +78,11 @@ int main() {
     // int v = entry["v"];
     // int lambd = entry["lambd"];
 
+    std::string filename =
+        OUT_DIR_RESULTS + fmt::format("/{:06}_{:06}_{:03}.json", n, r, t);
+
+    nlohmann::json out_values;
+
     Result current_c_res;
     Result current_q_res;
 
@@ -68,21 +90,15 @@ int main() {
       spdlog::info("Processing n {}, k {}, t {}, qc_block_size {}, is_kra {}, "
                    "is_red_factor_applied {}",
                    n, k, t, qc_block_size, is_kra, is_red_factor_applied);
-      current_c_res = 
+      current_c_res =
           c_isd_log_cost(n, k, t, qc_block_size, is_kra, is_red_factor_applied);
-      current_q_res = 
+      current_q_res =
           q_isd_log_cost(n, k, t, qc_block_size, is_kra, is_red_factor_applied);
-      nlohmann::json out_values;
-      out_values["C2"] = current_c_res;
-      out_values["Q2"] = current_q_res;
+      std::string is_kra_name = is_kra ? "KRA": "MRA";
+      out_values[is_kra_name]["C"] = current_c_res;
+      out_values[is_kra_name]["Q"] = current_q_res;
+    }
 
-      std::ostringstream oss;
-      oss << std::setw(6) << std::setfill('0') << n << "_" << std::setw(6)
-          << std::setfill('0') << r << "_" << std::setw(3) << std::setfill('0')
-          << t << "_" << std::setw(1) << is_kra;
-      std::string filename = "out/" + oss.str() + ".json";
-
-      // Write the JSON object to the file
       std::ofstream file(filename);
       if (file.is_open()) {
         file << std::fixed << std::setprecision(10)
@@ -92,7 +108,6 @@ int main() {
       } else {
         std::cerr << "Could not open the file!" << std::endl;
       }
-    }
   }
   return 0;
 }
